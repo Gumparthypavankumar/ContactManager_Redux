548ebbb9291e6877a0a33a17ba8d5548de9460f0
Gumparthy Pavan Kumar <gumparthypk@gmail.com>
2020-10-21 13:08:43 +0530

    ContactManager with Redux

---
+++ src/actions/contactActions.js
@@ -0,0 +1,56 @@
+import { GET_CONTACTS,DELETE_CONTACT,ADD_CONTACT,GET_CONTACT,UPDATE_CONTACT} from './types';
+import axios from 'axios';
+export const getContacts = ()=> (dispatch) => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res => dispatch ({
+        type:GET_CONTACTS,
+        payload:res.data
+    }))
+    .catch(err => console.log(err))
+}
+
+export const getContact = (id)=> (dispatch) => {
+    axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+    .then(res => dispatch ({
+        type:GET_CONTACT,
+        payload:res.data
+    }))
+    .catch(err => console.log(err))
+}
+
+export const deleteContact = (id) => (dispatch) => {
+    try{
+
+        axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then(res => 
+        dispatch({
+            type:DELETE_CONTACT,
+            payload:id
+        }))
+        .catch(err => console.log(err))
+    }
+    catch(e){
+        dispatch({
+            type:DELETE_CONTACT,
+            payload:id
+        })
+    }
+}
+
+export const addContact = (contact) => (dispatch) => {
+    axios.post('https://jsonplaceholder.typicode.com/users',contact)
+    .then(res => dispatch({
+        type:ADD_CONTACT,
+        payload:res.data
+    }))
+    .catch(err => console.log(err))
+}
+
+export const updateContact = (contact) => (dispatch) => {
+    axios.put(`https://jsonplaceholder.typicode.com/users/${contact.id}`,contact)
+    .then(res => dispatch({
+        type:UPDATE_CONTACT,
+        payload:res.data
+    }))
+    .catch(err => console.log(err))
+}
